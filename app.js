41a00cdc1d142abdd71e7a41ffbdf639016803cc
Pratham Bansal <bansalpratham.bt21cse@pec.edu.in>
2023-06-23 00:23:21 +0530

    Initial commit

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.post("/",function(req,res){
             console.log(JSON.parse(data));
         });
     });
-    // request.write(jsonData);
+    request.write(jsonData);
     request.end();
 });
 
@@ -52,7 +52,7 @@ app.post("/failure",function(req,res){
     res.redirect("/");
 });
 
-app.listen(process.env.PORT || 3000,function(req,res){ 
+app.listen(3000,function(req,res){ 
     console.log("The server is running on port 3000.");
 });
 
